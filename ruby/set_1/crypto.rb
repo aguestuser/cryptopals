a1d3fca5189bc963d6174f8ac13ce7ea787e7ff5
aguestuser <aguestuser@riseup.net>
2018-04-02 02:39:28 -0400

    do challenge 2 (fixed x_or)
    side-effects:
    * monkey patch String to have `decode_hex`
    * monkey patch Array to have `encode_hex`

--- ruby/set_1/crypto.rb
+++ ruby/set_1/crypto.rb
@@ -1,9 +1,29 @@
 require 'base64'
 
 module Crypto
+  class ::String
+    def hex_to_base64 # => string
+      [[self].pack('H*')].pack("m0")
+    end
+
+    def decode_hex # => Array<byte>
+      [self].pack("H*").bytes
+    end
+  end
+
+  class ::Array
+    def encode_hex # => string
+      self.map{ |b| b.to_s(16) }.join
+    end
+  end
+
   class << self
-    def hex_to_base64(hex_str)
-      [[hex_str].pack('H*')].pack("m0")
+    # string, string ->  string
+    def fixed_xor(hex1, hex2)
+      hex1.decode_hex
+        .zip(hex2.decode_hex)
+        .map{ |a,b| a ^ b }
+        .encode_hex
     end
   end
 end
--- ruby/set_1/crypto_spec.rb
+++ ruby/set_1/crypto_spec.rb
@@ -3,7 +3,15 @@ require_relative "./crypto"
 describe Crypto do
   # challenge 1
   it 'converts a hex string to a base64 string' do
-    expect(Crypto.hex_to_base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").
+    expect("49276d206b696c6c696e6720796f757220627261696e206c696b6520612" +
+           "0706f69736f6e6f7573206d757368726f6f6d".hex_to_base64).
       to eql "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+  end
+
+  # challenge 2
+  it 'performs the fixed xor of two hex strings' do
+    expect(Crypto.fixed_xor "1c0111001f010100061a024b53535009181c",
+                            "686974207468652062756c6c277320657965").
+      to eql "746865206b696420646f6e277420706c6179"
   end
 end

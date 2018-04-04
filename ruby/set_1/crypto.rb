c270c8f4c762de0c10e1e898296ecfed1534acd9
aguestuser <aguestuser@riseup.net>
2018-04-04 12:46:37 -0400

    WIP: setup for challenge 3 (single byte xor)

--- ruby/set_1/crypto.rb
+++ ruby/set_1/crypto.rb
@@ -1,6 +1,38 @@
 require 'base64'
 
 module Crypto
+
+  # TODO: get better frequencies
+  # this is from: https://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+  LETTER_FREQUENCIES = {
+    e: 12.02,
+    t: 9.10,
+    a: 8.12,
+    o: 7.68,
+    i: 7.31,
+    n: 6.95,
+    s: 6.28,
+    r: 6.02,
+    h: 5.92,
+    d: 4.32,
+    l: 3.98,
+    u: 2.88,
+    c: 2.71,
+    m: 2.61,
+    f: 2.30,
+    y: 2.11,
+    w: 2.09,
+    g: 2.03,
+    p: 1.82,
+    b: 1.49,
+    v: 1.11,
+    k: 0.69,
+    x: 0.17,
+    q: 0.11,
+    j: 0.10,
+    z: 0.07,
+  }
+
   class ::String
     def hex_to_base64 # => string
       [[self].pack('H*')].pack("m0")
@@ -24,6 +56,18 @@ module Crypto
         .zip(hex2.decode_hex)
         .map{ |a,b| a ^ b }
         .encode_hex
+    end
+  end
+
+  class SingleByteXOR
+    FREQUENCIES = {
+
+    }
+    class << self
+      # string -> string
+      def decrypt(hex)
+        hex
+      end
     end
   end
 end
--- ruby/set_1/crypto_spec.rb
+++ ruby/set_1/crypto_spec.rb
@@ -1,17 +1,49 @@
 require_relative "./crypto"
 
 describe Crypto do
-  # challenge 1
-  it 'converts a hex string to a base64 string' do
-    expect("49276d206b696c6c696e6720796f757220627261696e206c696b6520612" +
-           "0706f69736f6e6f7573206d757368726f6f6d".hex_to_base64).
-      to eql "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+  describe "challenge solution" do
+    it 'converts a hex string to a base64 string' do
+      expect("49276d206b696c6c696e6720796f757220627261696e206c696b6520612" +
+             "0706f69736f6e6f7573206d757368726f6f6d".hex_to_base64).
+        to eql "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    end
   end
 
-  # challenge 2
-  it 'performs the fixed xor of two hex strings' do
-    expect(Crypto.fixed_xor "1c0111001f010100061a024b53535009181c",
-                            "686974207468652062756c6c277320657965").
-      to eql "746865206b696420646f6e277420706c6179"
+  describe "challenge 2 solution" do
+    it 'performs the fixed xor of two hex strings' do
+      expect(Crypto.fixed_xor "1c0111001f010100061a024b53535009181c",
+                              "686974207468652062756c6c277320657965").
+        to eql "746865206b696420646f6e277420706c6179"
+    end
+  end
+
+  describe "challenge 3 solution" do
+=begin
+Single-byte XOR cipher
+
+The hex encoded string:
+1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+... has been XOR'd against a single character. Find the key, decrypt the message.
+
+You can do this by hand. But don't: write code to do it for you.
+How? Devise some method for "scoring" a piece of English plaintext.
+Character frequency is a good metric. Evaluate each output and choose the one with the best score.
+=end
+
+    # ASSUMPTIONS:
+    # 1. our frequency count is accurate
+    # 2. our
+
+    it 'counts the frequencies of characters in a string'
+
+    it 'scores a string based on how close the frequencies are to observed'
+
+    it 'compares the scores of all possible keys in the ASCII character space'
+
+    it 'decrypts a message encrypted with single-byte XOR cypter' do
+      skip
+      m = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+      expect(Crypto::SingleByteXOR.deccrypt(m)).to eql "???"
+    end
   end
 end
